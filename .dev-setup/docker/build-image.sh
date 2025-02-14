63d6216dab94d566f5878d39fe92ac6ec24e3447
Yonatan <yontan.mamo@ipa.fraunhofer.de>
2025-02-14 19:15:29 +0000

    comment out Dockerfile.run build commands

--- .dev-setup/docker/build-image.sh
+++ .dev-setup/docker/build-image.sh
@@ -5,7 +5,7 @@ docker build --file .dev-setup/docker/Dockerfile.base \
     --secret id=gitcreds,src=$HOME/.git-credentials \
     --tag robot_mindset_base:1.0 \
     .
-docker build --file .dev-setup/docker/Dockerfile.run \
-    --secret id=gitcreds,src=$HOME/.git-credentials \
-    --tag robot_mindset_run:1.0 \
-    .
+# docker build --file .dev-setup/docker/Dockerfile.run \
+#     --secret id=gitcreds,src=$HOME/.git-credentials \
+#     --tag robot_mindset_run:1.0 \
+#     .
